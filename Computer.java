c494290894d3f1785325fd755dbff6b77c94252c
kas-fsu <34460730+kas-fsu@users.noreply.github.com>
2020-04-21 17:19:06 -0400

    Update Computer.java

--- Computer.java
+++ Computer.java
@@ -1,13 +1,12 @@
-// in progress
 public class Computer extends Player {
     public Computer() {
     }
-
-    public Card makeMove(Card c) {
+    
+    public boolean makeMove(Card c) {
         for(Card x : hand) {
             String xType = x.getValue();
             String cType = c.getValue();
-
+            
             if((xType.startsWith("1") && cType.startsWith("1"))
                || (xType.startsWith("2") && cType.startsWith("2"))
                || (xType.startsWith("3") && cType.startsWith("3"))
@@ -16,10 +15,22 @@ public class Computer extends Player {
                || (xType.startsWith("6") && cType.startsWith("6"))
                || (xType.startsWith("7") && cType.startsWith("7"))
                || (xType.startsWith("8") && cType.startsWith("8"))
-               || (xType.startsWith("9") && cType.startsWith("9"))) {
+               || (xType.startsWith("9") && cType.startsWith("9"))
+               || (xType.startsWith("skip") && cType.startsWith("skip"))
+               || (xType.startsWith("rev") && cType.startsWith("rev"))
+               || (xType.startsWith("p2") && cType.startsWith("p2"))
+               || (xType.startsWith("wild"))) {
+                  lastDiscarded = x;
                   hand.remove(x);
-                  return x;
-            }
+                  
+                  if(getHandSize() == 0) {
+                      changeSaysUno(true);
+                  }
+                        
+                  return true;
+            } 
+            
+            return false;
         }
     }
 }
